b68383b80698c184166813959f5dce28d9d1a7ab
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-04-17 16:47:29 +0530

    feat: equality

--- hello.c
+++ hello.c
@@ -4,6 +4,13 @@ int main() {
     int a = 2;
     int b = 3;
     int c = a + b;
+
+    if(c == 5){
+        printf("equal");
+    } else{
+        printf("not equal");
+    }
+    
     printf("Hello, World!\n");
     return 0;
 }
